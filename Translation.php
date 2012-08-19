728467bcb9ca485437bf009a06ce8e6c25cf95a3
bugadani <bugadani@bugadani-inspiron>
2012-08-20 00:38:57 +0200

    Update headers. Added License file. Added method to add multiple strings at once.

--- Translation.php
+++ Translation.php
@@ -2,26 +2,9 @@
 
 /**
  * This file is part of the Miny framework.
+ * (c) Dániel Buga <daniel@bugadani.hu>
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version accepted by the author in accordance with section
- * 14 of the GNU General Public License.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * @package   Miny/Modules/Translation
- * @copyright 2012 Dániel Buga <daniel@bugadani.hu>
- * @license   http://www.gnu.org/licenses/gpl.txt
- *            GNU General Public License
- * @version   1.0-dev
+ * For licensing information see the LICENSE file.
  */
 
 namespace Modules\Translation;
@@ -51,7 +34,14 @@ class Translation
             $rule = preg_replace('/[^n0-9\w=\-+%<>]/', '', $rule);
             $this->rules[$name] = $rule;
         }
-        $this->strings = $this->strings + $loader->load($lang);
+        $this->strings = $loader->load($lang);
+    }
+
+    public function addStrings(array $strings)
+    {
+        foreach ($strings as $key => $string) {
+            $this->addString($key, $string);
+        }
     }
 
     public function addString($key, $string)
